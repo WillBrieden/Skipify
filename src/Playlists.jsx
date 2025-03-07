ce15072a1cf8c630e9582c0eda83bdc9a05c558d
William Brieden <briiedwil@gmail.com>
2025-03-07 13:08:27 -0600

    Adding buttons to allow displaying more playlists

--- src/Playlists.jsx
+++ src/Playlists.jsx
@@ -2,29 +2,37 @@ import React, { useEffect, useState } from "react";
 import PlaylistItem from "./PlaylistItem";
 
 function Playlists(props){
-    const token = props.token;
-    const [playlists, setPlaylists] = useState([]);
+    const [token, setToken] = useState(props.token);
+    const [fetchUrl, setFetchUrl] = useState("https://api.spotify.com/v1/me/playlists");
+    const [data, setData] = useState(null);
 
     useEffect(() => {
         async function fetchPlaylists(){
-            const response = await fetch("https://api.spotify.com/v1/me/playlists", {method: "GET", headers: { Authorization: 'Bearer ' + token}});
+            const response = await fetch(fetchUrl, {method: "GET", headers: { Authorization: 'Bearer ' + token}});
             const json = await response.json();
 
-            setPlaylists(json.items);
+            setData(json);
+            console.log(data)
         }
+        if(props.active){
+            fetchPlaylists();
+        }
+    }, [fetchUrl, token, props.active]);
 
-        fetchPlaylists();
-    });
-
-    if(!props.active){
+    if(!props.active || data == null){
         return null;
     }else{
         return (
             <div className="playlistsTab">
-                {playlists.length ? 
-                    playlists.map(playlist => <PlaylistItem images={playlist.images} name={playlist.name} owner={playlist.owner} public={playlist.public} tracks={playlist.tracks} url={playlist.url} uri={playlist.uri}></PlaylistItem> )
+                {data.items.length ? 
+                    data.items.map(playlist => <PlaylistItem images={playlist.images} name={playlist.name} owner={playlist.owner} public={playlist.public} tracks={playlist.tracks} url={playlist.url} uri={playlist.uri}></PlaylistItem> )
                     : 'No Playlists Found'
                 }
+                <div className="playlistButtons">
+                    <button className="btn-spotify-playlists" onClick={setFetchUrl(data.previous)}>Prev</button>
+                    <p>{data.limit} {data.offset}/{data.total}</p>
+                    <button className="btn-spotify-playlists" onClick={setFetchUrl(data.next)}>Next</button>
+                </div>
             </div>
         );
     }
