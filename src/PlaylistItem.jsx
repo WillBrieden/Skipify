0a10b0bbe62d137e75da00a0b5cd3e23272bd26e
William Brieden <briiedwil@gmail.com>
2025-03-11 14:26:38 -0500

    Update playlist text visuals

--- src/PlaylistItem.jsx
+++ src/PlaylistItem.jsx
@@ -4,10 +4,10 @@ function PlaylistItem(props){
     return(
         <div className="playlistItem">
             <img src={props.images[0].url} alt="" />
-            <p>{props.name}</p>
-            <p>{props.owner.display_name}</p>
-            <p>{props.public ? 'Public' : 'Private'}</p>
-            <p>{props.tracks.total} tracks</p>
+            <p className="playlistName">{props.name}</p>
+            <p className="playlistDisplay">{props.owner.display_name}</p>
+            <p className="playlistPublic">{props.public ? 'Public' : 'Private'}</p>
+            <p className="playlistTracks">{props.tracks.total} tracks</p>
         </div>
     );
 }
